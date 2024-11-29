269e43d1a2b97988af4c851268c771ca0f7f49a0
Khai0009 <huukhai890@gmail.com>
2024-11-29 16:24:28 +0700

    lam gon

--- src/app/Data.tsx
+++ src/app/Data.tsx
@@ -6,6 +6,9 @@ import { collection, getDocs, query } from 'firebase/firestore';
 const getBooks = async () => { 
   const q = query(collection(db, 'truyen')); 
   const querySnapshot = await getDocs(q); 
-  const bookData = querySnapshot.docs.map((doc) => ({ ID: doc.data().ID, Tentruyen: doc.data().Tentruyen, Gioithieu: doc.data().Gioithieu })); 
+  const bookData = querySnapshot.docs.map((doc) => 
+    ({ ID: doc.data().ID, 
+      Tentruyen: doc.data().Tentruyen, 
+      Gioithieu: doc.data().Gioithieu })); 
   return bookData; }; 
 export default getBooks
