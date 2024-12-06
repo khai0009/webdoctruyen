b4e2d32e4f31089c64a26fccd33ba2488501dcc6
Khai0009 <huukhai890@gmail.com>
2024-12-06 17:24:00 +0700

    sua lai anh

--- src/app/List/page.tsx
+++ src/app/List/page.tsx
@@ -28,7 +28,7 @@ books.map((book) => (
     <Link className="flex items-center space-x-4 rtl:space-x-reverse" href={`/List/Detail/${book.ID}`}>
     
        <div className="flex-shrink-0">
-          <img className="h-full w-20 object-cover md:h-full md:w-40" src={book.anh} alt={book.Tentruyen}/>
+          <Image className="h-full w-20 object-cover md:h-full md:w-40" src={book.anh} alt={book.Tentruyen} width={1080} height={1924}/>
        </div>
        <div className="flex-1 min-w-0">
           <p className="text-sm font-medium text-gray-900 truncate dark:text-white">
